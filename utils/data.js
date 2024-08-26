66721938ca8121b5cf93e3dcaf1c854365583e25
Hassan Zohaib <78634222+HassanZohaib121@users.noreply.github.com>
2024-08-27 01:26:30 +0500

    Update data.js

--- utils/data.js
+++ utils/data.js
@@ -1,20 +1,20 @@
 export const bankData = [
   {
     id: '1',
-    name: 'United Bank Limited (UBL)',
-    holder: 'Muhammad Iftikhar',
-    number: '0837 2454 95 586',
+    name: '',
+    holder: '',
+    number: '',
   },
   {
     id: '2',
-    name: 'Jazz Cash',
-    holder: 'Muhammad Iftikhar',
-    number: '+92 300 7242 408',
+    name: '',
+    holder: '',
+    number: '',
   },
   {
     id: '3',
-    name: 'Easy Paisa',
-    holder: 'Muhammad Iftikhar',
-    number: '+92 345 8655 021',
+    name: '',
+    holder: '',
+    number: '',
   },
 ]
