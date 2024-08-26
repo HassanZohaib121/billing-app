525c30f143ef954f53f6dfe28ca135e45e5aedb0
Hassan Zohaib <hassan.zohaib.184@gmail.com>
2024-08-27 01:10:20 +0500

    first commit

---
+++ utils/data.js
@@ -0,0 +1,20 @@
+export const bankData = [
+  {
+    id: '1',
+    name: 'United Bank Limited (UBL)',
+    holder: 'Muhammad Iftikhar',
+    number: '0837 2454 95 586',
+  },
+  {
+    id: '2',
+    name: 'Jazz Cash',
+    holder: 'Muhammad Iftikhar',
+    number: '+92 300 7242 408',
+  },
+  {
+    id: '3',
+    name: 'Easy Paisa',
+    holder: 'Muhammad Iftikhar',
+    number: '+92 345 8655 021',
+  },
+]
